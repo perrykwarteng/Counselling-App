e0a1b650a90372747dec30d9d1a132e01c0e1550
Perry John Kwarteng <perrykwarteng8@gmail.com>
2026-02-16 10:39:36 +0000

    linting issues

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,21 +1,3 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-// ];
-
-// export default eslintConfig;
-
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -29,16 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "no-unused-vars": "off",
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-      "no-console": "off",
-    },
-  },
+  
 ];
 
 export default eslintConfig;
